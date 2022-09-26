0f6d094eaafcfa390a1babf86950b370d109e3b8
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2022-09-26 07:12:36 +0100

    adding the ingredients field to the recipe table

--- models/recipe.py
+++ models/recipe.py
@@ -17,6 +17,7 @@ class Recipe(db.Model):
     updated_at = db.Column(db.DateTime(), nullable=False, 
                         server_default=db.func.now(), onupdate=db.func.now())
     cover_image = db.Column(db.String(100), default=None)
+    ingredients = db.Column(db.String(500), default=None)
     user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
         
     @classmethod
